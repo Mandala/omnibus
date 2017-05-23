6f82f72c8abe0d8bb9f966187e45e1809b5a17f4
Fadhli Dzil Ikram <fadhli@xtremax.com>
2017-05-23 10:40:29 +0700

    Fix incorrect package naming.

--- docs.go
+++ docs.go
@@ -8,7 +8,7 @@ Go. Whether you build REST API endpoint to full featured website or something
 in between, it got you covered. Omnibus subpackages is designed with
 modularity in mind so you can easily mix and match it with another package.
 */
-package main
+package omnibus
 
 // This package intentionally left blank to supress the no buildable files
 // warning on go-get.
